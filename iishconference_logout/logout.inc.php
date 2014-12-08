9c09c350779b485a72cb782bad2948780f9e7fba
kerim1 <kerim.meijer@gmail.com>
2014-12-08 13:00:37 +0100

    Fix logout

--- iishconference_logout/logout.inc.php
+++ iishconference_logout/logout.inc.php
@@ -22,20 +22,20 @@ function conference_logout_form($form, &$form_state) {
  * Implements hook_form_submit
  */
 function conference_logout_form_submit($form, &$form_state) {
-	$_SESSION["conference"]["user_id"] = null;
-	$_SESSION["conference"]["user_email"] = null;
+	$_SESSION['iish_conference']['user_id'] = null;
+	$_SESSION['iish_conference']['user_email'] = null;
 
-	$_SESSION["conference"]["login_default_email_existingusers"] = null;
-	$_SESSION["conference"]["login_default_email_newusers"] = null;
+	$_SESSION['iish_conference']['login_default_email_existingusers'] = null;
+	$_SESSION['iish_conference']['login_default_email_newusers'] = null;
 
-	$_SESSION["conference"]["hasFullRights"] = null;
-	$_SESSION["conference"]["isNetworkChair"] = null;
-	$_SESSION["conference"]["isChair"] = null;
-	$_SESSION["conference"]["isOrganiser"] = null;
-	$_SESSION["conference"]["isCrew"] = null;
+	$_SESSION['iish_conference']['hasFullRights'] = null;
+	$_SESSION['iish_conference']['isNetworkChair'] = null;
+	$_SESSION['iish_conference']['isChair'] = null;
+	$_SESSION['iish_conference']['isOrganiser'] = null;
+	$_SESSION['iish_conference']['isCrew'] = null;
 
-	$_SESSION["conference"]["user"] = null;
-	$_SESSION["conference"]["participant"] = null;
+	$_SESSION['iish_conference']['user'] = null;
+	$_SESSION['iish_conference']['participant'] = null;
 
 	unset($_SESSION['storage']);
 
